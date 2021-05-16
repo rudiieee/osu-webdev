1a14d1b6fd12d6b2fb7d8366ff43a01ccb43443c
Rodolfo <rodolfo.olivares@ibm.com>
2021-05-16 10:17:44 -0500

    port

--- getAndPost.js
+++ getAndPost.js
@@ -5,7 +5,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 9565);
 
 app.get('/',function(req,res){
   res.render('home.handlebars') //We can omit the .handlebars extension as we do below
@@ -28,5 +28,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
